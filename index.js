c6983bb5b4947ce3e6214e1a97972933a83a70e1
btoner <ben.toner@wework.com>
2018-10-01 19:42:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Code your solution in this file.
 function lowerCaseDrivers(drivers){
-  return drivers.map( driver => driver.toLowerCase() )
+  return drivers.map( driver => driver.toLowerCase() );
 }
 
 function nameToAttributes(drivers){
   var firstName = drivers.split(' ');
   var lastName = drivers.split(' ');
-  return drivers.map( driver => driver.firstName: + driver.lastName: )
+  return drivers.map( driver => firstName + lastName );
 }
